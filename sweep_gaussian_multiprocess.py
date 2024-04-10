00611dd7a15bf74c828e9caa598c6fcb1f45a942
YufengJin <jyf931118@gmail.com>
2024-04-10 16:32:50 +0200

    added a Multiprocessing Wandb Sweep Agent

--- sweep_gaussian_multiprocess.py
+++ sweep_gaussian_multiprocess.py
@@ -4,10 +4,13 @@ import numpy as np
 import yaml
 import json
 import imageio
+import wandb
+import time
+import multiprocessing
 from gs_runner import GaussianSplatRunner
 from PIL import Image
-import matplotlib.pyplot as plt
 
+# preprocess bop dataset
 # load from bop dataset
 dataRootDir = './bop_outputs/bop_data/ycbv/train_pbr/000000'
 
@@ -17,13 +20,6 @@ cameraInfo = json.load(open(dataRootDir + '/scene_camera.json', 'r'))
 K = np.asarray(cameraInfo['0']['cam_K'], dtype=np.float64).reshape(3, 3)
 
 cameraPoses = json.load(open(dataRootDir + '/scene_gt.json', 'r'))
-
-def center_scale_camer_poses(cam_poses):
-    # TODO from pointcloud
-    center = np.mean(cam_poses[:, :3, 3], axis = 0)
-    scale = 1.5/0.3
-    cam_poses[:, :3, 3] = (cam_poses[:, :3, 3] - center) * scale
-    return cam_poses
 
 camPoses = []
 rgbs = []
@@ -69,30 +65,9 @@ rgbs = np.asarray(rgbs)
 depths = np.asarray(depths)
 masks = np.asarray(masks)
 
-
 # initialize GaussianSplatRunner
 cfg = yaml.safe_load(open('gs_runner_config.yml', 'r'))
 
-import wandb
-
-sweep_config = {
-    "method": "random",
-    "metric": {"goal": "minimize", "name": "total"},
-    "parameters": {
-        "position_lr_init": {'distribution': 'uniform',"max": 0.001, "min": 0.00001},
-        "position_lr_final": {'distribution': 'uniform', "max": 0.0001, "min": 0.000001},
-        "position_lr_delay_unit": {'distribution': 'uniform', "max": 0.1, "min": 0.001},
-        "feature_lr": {'distribution': 'uniform', "max": 0.1, "min": 0.001},
-        "opacity_lr": {'distribution': 'uniform', "max": 0.1, "min": 0.001},
-        "scaling_lr": {'distribution': 'uniform', "max": 0.1, "min": 0.001},
-        "rotation_lr": {'distribution': 'uniform', "max": 0.01, "min": 0.0001},
-        "lambda_dssim": {'distribution': 'uniform', "max": 1, "min": 0},
-        "densification_interval": {'values': [50, 100, 200]},
-        "opacity_reset_interval": {'values': [1000, 3000, 5000]},
-        "densify_from_iter": {'values': [200, 500, 1000]},
-        "densify_until_iter": {'values': [10000, 15000, 20000]},
-    }
-    }
 
 def train(config=None):
         with wandb.init(
@@ -125,8 +100,53 @@ def train(config=None):
             gsRunner = GaussianSplatRunner(gs_cfg, colors=rgbs, poses=camPoses, frame_ids=frameIds, K=K,depths= depths, masks=masks, wandb=run)
             gsRunner.train()
 
-sweep_id = wandb.sweep(sweep_config, project='Gaussian Splatting Analysis')
-wandb.agent(sweep_id, function=train, count=2)
+def worker(num, sweep_id, count=1):
+    wandb.agent(sweep_id, function=train, count=count)
 
 
+def center_scale_camer_poses(cam_poses):
+    # TODO from pointcloud
+    center = np.mean(cam_poses[:, :3, 3], axis = 0)
+    scale = 1.5/0.3
+    cam_poses[:, :3, 3] = (cam_poses[:, :3, 3] - center) * scale
+    return cam_poses
 
+if __name__ == '__main__':
+    sweep_config = {
+        "method": "random",
+        "metric": {"goal": "minimize", "name": "total"},
+        "parameters": {
+            "position_lr_init": {'distribution': 'uniform',"max": 0.001, "min": 0.00001},
+            "position_lr_final": {'distribution': 'uniform', "max": 0.0001, "min": 0.000001},
+            "position_lr_delay_unit": {'distribution': 'uniform', "max": 0.1, "min": 0.001},
+            "feature_lr": {'distribution': 'uniform', "max": 0.1, "min": 0.001},
+            "opacity_lr": {'distribution': 'uniform', "max": 0.1, "min": 0.001},
+            "scaling_lr": {'distribution': 'uniform', "max": 0.1, "min": 0.001},
+            "rotation_lr": {'distribution': 'uniform', "max": 0.01, "min": 0.0001},
+            "lambda_dssim": {'distribution': 'uniform', "max": 1, "min": 0},
+            "densification_interval": {'values': [50, 100, 200]},
+            "opacity_reset_interval": {'values': [1000, 3000, 5000]},
+            "densify_from_iter": {'values': [200, 500, 1000]},
+            "densify_until_iter": {'values': [10000, 15000, 20000]},
+        }
+        }
+
+    sweep_id = wandb.sweep(sweep_config, project='Gaussian Splatting Analysis')
+    num_processes = 15  # Number of parallel processes
+    each_count = 200
+
+    # Create a list to hold the process objects
+    processes = []
+    multiprocessing.set_start_method('spawn') 
+
+    for i in range(num_processes):
+        p = multiprocessing.Process(target=worker, args=(i, sweep_id, each_count))
+        processes.append(p)
+        p.start()
+
+    # Join all the processes
+    for p in processes:
+        p.join()
+
+    print("All processes have finished.")
+   
